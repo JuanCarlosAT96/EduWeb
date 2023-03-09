9c6f6fdf8528f341b4d129e85882c2df0e99f939
juan <juan.alvarez193@tectijuana.edu.mx>
2023-03-09 00:30:11 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img src="../src/assets/logo.svg" alt="">
+
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<style></style>
